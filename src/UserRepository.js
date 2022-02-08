6635d2a050c7a32d9fdd298e853cf8a4225a9278
Rio Foster <friotious@gmail.com>
2022-02-07 19:56:41 -0800

    Add UserRepo class and displayUserData method

--- src/UserRepository.js
+++ src/UserRepository.js
@@ -1,19 +1,36 @@
+import userData from "./data/users"
+
+
 class UserRepository {
   //hold onto all user objects
   //parameter to pass user Data
-  constructor(users) {
-    this.users = users;
+
+  constructor(userData) {
+    this.userData = userData;
+    //this.users = []
   }
+
+  //createUsers() {
+  // this.users = userData.map(user => new User(user.id,
+  //     user.name, user.address, user.email, user.strideLength,
+  //     user.dailyStepGoal, user.friends))
+  // }
   //2 methods
   //what is user data passing in // ID
-  displayUserData(userID) {
 
+  displayUserData(userID) {
+    //  const users = this.userData
+    // let userInfo = users.find(user => user.id === userID )
+     //console.log(userData)
+
+    let userInfo = this.userData.find(user => user.id === userID);
+    return userInfo;
   }
 
   //average step goal among all users
-  calculateAvgStepGoal() {
-
-  }
- }
+  calculateAvgStepGoal() {}
+}
 
 export default UserRepository;
+
+//let cards = prototypeQuestions.map(card => new Card(card.id, card.question, card.answers, card.correctAnswer))
--- test/UserRepository-test.js
+++ test/UserRepository-test.js
@@ -1,8 +1,27 @@
-import { expect } from 'chai';
-import UserRepository from '../src/UserRepository';
+import { expect } from "chai";
+import UserRepository from "../src/UserRepository";
+import userData from "../src/data/users"
 
-describe('User Repository', () => {
-  it('should be a function', function () {
-    expect(UserRepository).to.be.a('function');
+let userRepo = new UserRepository(userData);
+// let users = userData.map(user => new User(user.id,
+//   user.name, user.address, user.email, user.strideLength,
+//   user.dailyStepGoal, user.friends))
+
+describe("User Repository", () => {
+  it("should be a function", function () {
+    expect(UserRepository).to.be.a("function");
+  });
+
+  it("should store all user objects", function () {
+    //userRepo.createUsers()
+    expect(userRepo.userData).to.deep.equal(userData);
+  });
+
+  it("should display user data", function () {
+    expect(userRepo.displayUserData(1)).to.deep.equal(userRepo.userData[0]);
+  });
+
+  it("should calculate the average step goal", function () {
+    expect(userRepo.calculateAvgStepGoal()).to.deep.equal(6700);
   });
 });
