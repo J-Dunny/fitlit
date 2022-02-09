6b9dfeb1a086043e17eaa9fc676940f4c3a98afc
Ronald Head <ronaldlhead@gmail.com>
2022-02-08 17:45:49 -0700

    Clean up scripts.js

--- src/scripts.js
+++ src/scripts.js
@@ -1,5 +1,3 @@
-// This is the JavaScript entry file - your code begins here
-// Do not delete or rename this file ********
 import './css/styles.css';
 import './images/turing-logo.png'
 import userData from './data/users';
@@ -9,22 +7,11 @@ const userName = document.getElementById("userName");
 const stepGoal = document.getElementById("stepGoal");
 const avgStepGoal = document.getElementById("avgStepGoal");
 
-console.log(userData,"<>>>>userData")
 const userRepo = new UserRepository(userData);
-console.log(userRepo)
-
 const firstUser = userRepo.userData[0]
-console.log(firstUser)
 
 userName.innerText = `Welcome ${firstUser.firstName()}`;
 
 stepGoal.innerText = `Step Goal: ${firstUser.dailyStepGoal}`;
 
 avgStepGoal.innerText = `Average Step Goal: ${userRepo.calculateAvgStepGoal()}`;
-// An example of how you tell webpack to use a CSS file
-
-// An example of how you tell webpack to use an image (also need to link to it in the index.html)
-
-console.log('This is the JavaScript entry file - your code begins here.');
-
-// An example of how you tell webpack to use a JS file
